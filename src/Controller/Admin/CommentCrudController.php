91e457c3f36068cb3034c7adf5312db2bc609fb9
[nom] <stellon.jonathan@gmail.com>
2022-06-15 10:26:01 +0200

    security ok

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -4,12 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Comment;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
-use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
@@ -28,8 +29,12 @@ class CommentCrudController extends AbstractCrudController
         yield TextEditorField::new('text');
         yield EmailField::new('email');
         yield AssociationField::new('conference');
-        yield TextField::new('photoFilename');
         yield DateTimeField::new('createdAt')->hideOnForm();
+        yield ImageField::new('photoFilename')
+            ->setBasePath('/uploads/photos')
+            ->setLabel('Photo')
+            ->onlyOnIndex()
+        ;
     }
 
     public function configureCrud(Crud $crud): Crud
